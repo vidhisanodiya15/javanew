44e104475289447e55ac875c52120826f9a221a0
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-01-31 23:38:55 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,6 @@
+public class string {
+    public static void main(String[] args){
+        String txt= "ADBFJVFBVDFJVBNDFKJVNBFKJVN";
+        System.out.println("The length of the txt string is:"+txt.length());
+    }
+}
---
+++ typecasting.java
@@ -0,0 +1,14 @@
+public class typecasting{
+    public static void main(String[] args){
+        // int myInt=9;
+        // double myDouble=myInt ;
+        // System.out.println(myInt);
+        // System.out.println(myDouble);
+
+        double myDouble =9.76d;
+        int myInt=(int) myDouble;
+        System.out.println(myDouble);
+        System.out.println(myInt);
+    }
+
+}
