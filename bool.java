d67115f7a4653c1e4df437007788d671eef5e977
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-02-02 23:29:40 +0530

    Add files via upload

---
+++ bool.java
@@ -0,0 +1,15 @@
+public class bool {
+    public static void main(String[] args){
+        // boolean isJavaFun = true;
+        // boolean isFishTasty = false;
+        // System.out.println(isJavaFun);
+        // System.out.println(isFishTasty);
+
+        int x=13;
+        int y=34;
+        System.out.println(x>y);
+
+
+    }
+    
+}
---
+++ specialchar.java
@@ -0,0 +1,8 @@
+public  class specialchar {
+    public static void main(String[] args){
+        // String txt = ("We are so-called virgin ");
+        // System.out.println(txt);
+        // System.out.println(Math.max(5,10));
+        System.out.println(Math.sqrt(423));
+    }
+}
