fe3a7513c8b02e86c18246fc74bb22d789673721
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-02-04 20:56:51 +0530

    Add files via upload

---
+++ day5.java
@@ -0,0 +1,21 @@
+public class day5 {
+    public static void main(String[] args){
+        // if (20>18){
+        // System.out.println("20 is greater than 18");
+        // int x = 34;
+        // int y = 24;
+        // if (x>y){
+           // System.out.println("x is greater than y");
+int time =20;
+    if (time<13){
+            System.out.println("Good day.");
+
+}else{
+System.out .println("Good eveninng.");
+    }
+        }
+
+    }
+
+    
+
