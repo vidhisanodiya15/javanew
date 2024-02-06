9731f6c0116190c868bbd856f6e507e329c3c5d2
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-02-06 23:21:36 +0530

    Add files via upload

---
+++ day7.java
@@ -0,0 +1,16 @@
+
+public class day7{
+    public static void main(String[] args){
+        // int i =0;
+        // do{
+        //     System.out.println(i);
+        //     i++;
+        // }
+        // while (i<5);
+        // for(int i=0;i<5;i++){
+        //     System.out.println(i);        }
+        for (int i=0; i<=10; i=i+2){
+            System.out.println(i);
+        }
+    }
+}
