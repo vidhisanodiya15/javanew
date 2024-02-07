5eb5124a33f96900d11532d0b66f7529a3b208df
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-02-07 22:41:00 +0530

    Add files via upload

--- main.java
+++ main.java
@@ -1,43 +1,8 @@
-public class main {
+public class main{
     public static void main(String[] args){
-        // int day=4;
-        // switch(day){
-        //     case 1:
-        //     System.out.println("Monday");
-        //     break;
-        // case 2:
-        //     System.out.println("Tuesday");
-        //     break;
-        // case 3:
-        //     System.out.println("Wednesday");
-        //     break;
-        // case 4:
-        //     System.out.println("Thursday");
-        //     break;
-        // case 5:
-        //     System.out.println("Friday");
-        //     break;
-        // case 6:
-        //     System.out.println("Satudrday");
-        //     break;
-        // case 7:
-        //     System.out.println("Sunday");
-        //     break;
-            
-
-        // }
-        int day =4;
-        switch(day){
-            case 6:
-            System.out.println("Today is Saturday");
-            break;
-            case 7:
-            System.out.println("Today is Sunday");
-            break;
-            default:
-            System.out.println("lookin forward for the weekand");
-
+        String[] cars= {"Volvo","BMW","ALTO"};
+        for(String i : cars){
+            System.out.println(i);
         }
     }
-    
 }
