fe55d519c7d8f741f0907c3a8b9c16a48f938891
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-02-09 22:55:10 +0530

    Add files via upload

--- main.java
+++ main.java
@@ -1,24 +1,8 @@
 public class main{
     public static void main(String[] args){
-        // for (int i=0; i<10 ;i++){
-        //     if(i==4){
-        //         break;
-        //     }
-        //     System.out.println(i);
-        // }
-        // for(int i =0; i<10;i++){
-        //     if(i==4){
-        //         continue;
-        //     }
-        //     System.out.println(i);
-        // }
-        int i=0;
-        while (i<10){
-            System.out.println(i);
-            i++;
-            if(i==4){
-                break;
-            }
-        }
+        String[] cars={"Volvo","BMW","Ford"};
+    //     System.out.println(cars[0]);
+    cars[0]="Opel";
+    System.out.println(cars[0]);
     }
 }
