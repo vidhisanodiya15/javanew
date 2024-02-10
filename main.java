fa914693b3000db63b3fe88ceb956417b10b6c86
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-02-10 22:59:06 +0530

    Add files via upload

--- main.java
+++ main.java
@@ -1,8 +1,10 @@
 public class main{
     public static void main(String[] args){
-        String[] cars={"Volvo","BMW","Ford"};
-    //     System.out.println(cars[0]);
-    cars[0]="Opel";
-    System.out.println(cars[0]);
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+for (int i = 0; i < cars.length; i++) {
+System.out.println(cars[i]);
+}
+
+
     }
 }
