951d803da204eaba5b5c34d651224220f68c90b4
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-02-11 22:47:15 +0530

    Add files via upload

--- main.java
+++ main.java
@@ -1,10 +1,9 @@
 public class main{
     public static void main(String[] args){
         String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-for (int i = 0; i < cars.length; i++) {
-System.out.println(cars[i]);
-}
-
-
+cars[0] = "Opel";
+System.out.println(cars[3]);
+// Now outputs Opel instead of Volvo
     }
 }
+    
