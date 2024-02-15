fdd05c171b5f8270944ac8bfdadda4a79955a0dc
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-02-15 22:52:03 +0530

    Add files via upload

---
+++ oop.java
@@ -0,0 +1,8 @@
+public class oop{
+    int x =7;
+    public static void main(String[] args){
+        oop myObj = new oop();
+        System.out.println(myObj.x);
+
+    }
+}
---
+++ oop2.java
@@ -0,0 +1,11 @@
+public class oop2 {
+    int x =5;
+    public static void main(String[] args){
+        oop2 myObj1 = new oop2();
+        oop2 myObj2 = new oop2();
+        System.out.println(myObj1.x);
+        System.out.println(myObj2.x);
+
+
+    }
+}
