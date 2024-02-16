5c753aa082cc674118729431faad3ae89e26df64
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-02-17 00:50:07 +0530

    Add files via upload

---
+++ loop2.java
@@ -0,0 +1,13 @@
+public class loop2 {
+    public static void main(String[] args){
+        int i=0;
+        // while(i<11){
+        //     System.out.print(i=i+1);
+        // } 
+        do{
+            System.out.println(i=i+1);
+        }while(i<11);
+        
+    }
+
+}
---
+++ loops.java
@@ -0,0 +1,17 @@
+// public class loops {
+//     public static void main(String[] args){
+//         // for(int counter = 0; counter <24; counter = counter + 1){
+//         //     System.out.println("Hello");
+//         // }
+//         for(int counter = 0;counter <11;counter++){
+//             System.out.print(counter);
+//         }
+//     }
+// }
+public class loops{
+    public static void main(String[] args){
+        for(int i =0; i<11;i++){
+            System.out.println(i);
+        }
+    }
+}
