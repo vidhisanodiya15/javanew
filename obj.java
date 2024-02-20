646340b2ec4e6edd97acc13f51d67a28afe2de50
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-02-20 23:26:55 +0530

    Add files via upload

---
+++ obj.java
@@ -0,0 +1,12 @@
+public class obj{
+    int x = 5;
+
+    public static void main(String[] args){
+    
+        obj myObj = new obj();
+            System.out.println(myObj.x);
+            }
+        }
+        
+
+    
