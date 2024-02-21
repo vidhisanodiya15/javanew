6209f765ad7da64a7d17541875485901b5b6ca4c
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-02-21 23:34:55 +0530

    Add files via upload

--- main.java
+++ main.java
@@ -1,10 +1,9 @@
-public class main{
-   static void myMethod(String fname){
-    System.out.println(fname + " Refsnes");
-   }
-   public static void main(String[] args){
-    myMethod("Liam");
-    myMethod("jenny");
-    myMethod("anu");
-   }
+public class main {
+    static void myMethod() {
+    System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+    myMethod();
+    }
 }
