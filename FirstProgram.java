a4a767e60271ddeaf790df1e4b6b791cbfc829d6
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-02-22 13:01:43 +0530

    Add files via upload

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,28 +1,13 @@
-// public class  FirstProgram{
-//     void getValues(int a){
-//         System.out.println("value of a= "+a);
-//     }
-//     void getValues(int a,int b){
-//         System.out.println("values of a & b="+(a+b));
-
-//     }
-//     public static void main(String[] args){
-//         FirstProgram abc= new FirstProgram();
-//         abc.getValues(27);
-//         abc.getValues(28,12);
-//     }
-// }
-public class  FirstProgram{
-    void getValues(int a){
-        System.out.println("value of a= "+a);
+class zu{
+    int rollno=56;
+    String name="Vidhi";
+    void getValues(){
+        System.out.println("Name=" + name + "Rollno=" + rollno);
     }
-    void getValues(int a,int b,int c){
-        System.out.println("values of a & b & c ="+(a+b+c));
-
-    }
+}
+public class FirstProgram extends zu{
     public static void main(String[] args){
-        FirstProgram abc= new FirstProgram();
-        abc.getValues(27);
-        abc.getValues(28,12,34);
+        FirstProgram r=new FirstProgram();
+        r.getValues();
     }
 }
