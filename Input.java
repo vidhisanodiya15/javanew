3fbd597e13fcb12e8777c00b650384d6005b1d0a
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-02-27 21:58:44 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter some input:");
+        int rollno = input.nextInt();
+        System.out.println("Your roll number is"+rollno);
+
+    }
+    
+}
---
+++ primitive.java
@@ -0,0 +1,13 @@
+public class primitive{
+    public static void main(String[] args){
+        int rollno =64;
+
+        //String is not primitive
+        char letter ='r';
+        float marks =4.4f;
+        double largeDecimalNumbers =233434.43;
+        long largeInteger =2222222222232422L;
+        boolean check =true;
+        
+    }
+}
