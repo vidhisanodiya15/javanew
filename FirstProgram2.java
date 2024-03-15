3509f055740d127b52dc8a0693e4c77a48d3c941
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-03-15 13:52:32 +0530

    Add files via upload

--- FirstProgram2.java
+++ FirstProgram2.java
@@ -1,19 +1,16 @@
-class zu{
-    int rollno=56;
-    String name="Vidhi";
-    void getValues(){
-        System.out.println("Name=" + name + "Rollno=" + rollno);
+public class FirstProgram2 {
+    private int data=20;
+    void display(){
+        class local{
+            void msg(){
+                System.out.println(data);
+            }
+            local r=new local();
+            r.msg();
+        }
     }
-}
-class aman extends zu{
-    void getDetails(){
-        System.out.println("Function from aman");
-
-    }
-}
-public class FirstProgram2 extends zu{
-    public static void main(String[] args){
-        FirstProgram2 r=new FirstProgram2();
-        r.getValues();
+    public static void main(String[] args) {
+        FirstProgram2 obj=new FirstProgram2();
+        obj.display();
     }
 }
