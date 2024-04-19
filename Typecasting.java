14c63bfa126d06cc715a2ca8e84ba06b0014516f
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-04-19 16:27:05 +0530

    Add files via upload

---
+++ Typecasting.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Typecasting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // float num = input.nextFloat();
+        
+        int num = (int)(45.65f);
+
+        System.out.println(num);
+    }
+    
+}
