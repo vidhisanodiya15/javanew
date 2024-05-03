885fe6595b9e20809a2a86e65b8478816707519e
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-05-03 23:32:13 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,13 @@
+public class Array{
+    public static void main(String[] args) {
+        int[] marks= new int[3];
+        marks[0]=32;
+        marks[1]=36;
+        marks[2]=98;
+        for(int i=0; i<3; i++){
+            System.out.println(marks[i]);
+        }
+        
+
+    }
+}
