4c19250b8663439290dffb307c14eaa0e7fb5ee6
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-05-06 15:21:31 +0530

    Add files via upload

---
+++ New.java
@@ -0,0 +1,19 @@
+public class New{
+    public static void main(String[] args) {
+        // Scanner sc=new Scanner(System.in);
+        // String name = sc.next();
+        // System.out.println("Your name is:"+ name);
+        //concatenation
+        String firstname="vidhi";
+        String lastname="sanodiya";
+        String fullname=firstname+lastname;
+        System.out.println(fullname);
+        for(int i=0;i<fullname.length();i++){
+            System.out.println(fullname.charAt(i));
+
+        }
+
+
+
+    }
+}
