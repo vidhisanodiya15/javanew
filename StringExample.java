be79991d8e3fe94b66122211bb3496040c3121b6
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-05-29 21:35:48 +0530

    StringExample.java

---
+++ StringExample.java
@@ -0,0 +1,17 @@
+// Java Program to demonstrate
+// String
+public class StringExample {
+
+    // Main Function
+    public static void main(String args[])
+    {
+        String str = new String("example");
+        // creating Java string by new keyword
+        // this statement create two object i.e
+        // first the object is created in heap
+        // memory area and second the object is
+        // created in String constant pool.
+
+        System.out.println(str);
+    }
+}
