7d9b299a882e1d24bc5d2f1edba303cbc8b99e79
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-06-07 18:13:26 +0530

    Add files via upload

---
+++ Adult.java
@@ -0,0 +1,9 @@
+public class Adult {
+    public static void main(String[] args) {
+        boolean isAdult = false;
+        if(!isAdult)
+            System.out.println("is adult");
+        else
+            System.out.println("not adult");
+    }
+}
---
+++ age.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class age {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("write your age: ");
+        int age = sc.nextInt();
+        System.out.println(age);
+    }
+}
